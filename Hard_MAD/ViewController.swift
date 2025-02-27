9e58e01bb5b55a2954de578863bcb98e089238b2
Nikita Skazhutin <skazhutinnikita@gmail.com>
2025-02-27 12:57:10 +0700

    Initial Commit

---
+++ Hard_MAD/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hard_MAD
+//
+//  Created by dark type on 27.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Hard_MADUITests/Hard_MADUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Hard_MADUITestsLaunchTests.swift
+//  Hard_MADUITests
+//
+//  Created by dark type on 27.02.2025.
+//
+
+import XCTest
+
+final class Hard_MADUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
